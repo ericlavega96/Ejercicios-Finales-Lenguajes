41db680ea883af92ca548cad9e2d85d64daf832b
Eric Nunez <ericlavega96@gmail.com>
2018-11-25 14:18:48 -0400

    Subiendo ejercicio de Fibonacci

---
+++ src/main/java/Fibonacci.java
@@ -0,0 +1,27 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        boolean exit = false;
+        do{
+            Scanner scanner = new Scanner(System.in);
+            long n = 0;
+            System.out.println("Introduzca un número: ");
+            try{
+                n = scanner.nextLong();
+                System.out.println("El fibonacci de " + n + " es: " + fibonnacciRecursivo(n));
+                exit = true;
+            }catch (InputMismatchException ime){
+                System.out.println("Debe de introducir un número");
+            }
+        }while (!exit);
+    }
+
+
+    static long fibonnacciRecursivo(long n){
+    if(n <= 1)
+        return 1;
+    return (fibonnacciRecursivo(n - 1) + fibonnacciRecursivo(n - 2));
+    }
+}
